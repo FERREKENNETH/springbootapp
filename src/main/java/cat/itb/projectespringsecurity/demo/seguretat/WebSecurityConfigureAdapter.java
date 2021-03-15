3f7ae8adbd6716e28993371415f159c2ff8dca6e
sjo <sjo@sjo.com>
2021-03-15 19:06:09 +0100

    Añadimos paginas html

--- src/main/java/cat/itb/projectespringsecurity/demo/seguretat/WebSecurityConfigureAdapter.java
+++ src/main/java/cat/itb/projectespringsecurity/demo/seguretat/WebSecurityConfigureAdapter.java
@@ -1,23 +1,31 @@
 package cat.itb.projectespringsecurity.demo.seguretat;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
 @EnableWebSecurity
 class ConfiguracioSeguretatWeb extends WebSecurityConfigurerAdapter {
 
-    
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
         auth
                 .inMemoryAuthentication()
-                .passwordEncoder(NoOpPasswordEncoder.getInstance())
+                .passwordEncoder(passwordEncoder())
                 .withUser("Montse")
-                .password("secret")
+                .password(passwordEncoder().encode("secret"))
                 .roles("vip");
     }
 }
