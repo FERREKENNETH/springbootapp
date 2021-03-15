d86bd6bb9f882b142b44e7fa0c521ea488f58dac
sjo <sjo@sjo.com>
2021-03-15 18:52:01 +0100

    Initial commit

---
+++ src/main/java/cat/itb/projectespringsecurity/demo/ProjecteDeSpringSecurityApplication.java
@@ -0,0 +1,13 @@
+package cat.itb.projectespringsecurity.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjecteDeSpringSecurityApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProjecteDeSpringSecurityApplication.class, args);
+    }
+
+}
---
+++ src/test/java/cat/itb/projectespringsecurity/demo/ProjecteDeSpringSecurityApplicationTests.java
@@ -0,0 +1,13 @@
+package cat.itb.projectespringsecurity.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjecteDeSpringSecurityApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
